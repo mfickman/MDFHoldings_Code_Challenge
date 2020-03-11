3dcad9e70a8178a55f52cc70ca99c9fc6518c9d1
mfickma <mfickman@duqlight.com>
2020-03-11 13:18:51 -0400

    version history for Mark Fickman MDF Code Challenge

--- MarkFickmanMDFHoldingFlaskPythonCodeChallenge/static/js/timer.js
+++ MarkFickmanMDFHoldingFlaskPythonCodeChallenge/static/js/timer.js
@@ -1,14 +1,25 @@
+//create count up timer in javascript
+
+/*
+Count up timer based on basing the activation time for each
+unique call in the active call log
+
+By: Mark Fickman
+*/
+
 var startTimer = function(item) {
+
+    //initialize startDateTime variable and holders for start and timer
     console.log("item: ", item)
     var startDateTime = new Date(item.startTime)
     console.log("startDateTime", startDateTime)
     var startStamp = startDateTime.getTime();
-
     var newDate = new Date();
     var newStamp = newDate.getTime();
-
     var timer;
 
+    //create function to update date time show
+    //utilize necessary math functions
     function updateClock() {
         newDate = new Date();
         newStamp = newDate.getTime();
@@ -21,7 +32,7 @@ var startTimer = function(item) {
         var m = Math.floor(diff/(60));
         diff = diff-(m*60);
         var s = diff;
-
+        
         document.getElementById(item.id).innerHTML = d+" day(s), "+h+" hour(s), "+m+" minute(s), "+s+" second(s) active";
     }
 
